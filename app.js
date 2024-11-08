532f07aa7b9968552d5a7c72eec5b6537e0a2159
Vinícius Roulim Gonçalves <130313804+Vinicius-Goncalves0@users.noreply.github.com>
2024-11-08 11:36:43 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+import { Game } from './controller/Game.js';
+import { UI } from './viewer/UI.js';
+
+
+// Uso das classes
+const minNumbS = 1;
+const maxNumbS = 100;
+const game = new Game(minNumbS, maxNumbS);
+UI.gameStartupText(minNumbS, maxNumbS);
+
+// Eventos dos botões
+document.getElementById('kick').addEventListener('click', () => {
+    let kick = document.querySelector('input').value;
+    game.kickCheck(kick);
+ });
+
+document.getElementById('restart').addEventListener('click', () => {
+    game.restartGame();
+});
